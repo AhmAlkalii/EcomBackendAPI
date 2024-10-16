b207d84e8dba94111b551b54cd92214a1d699a72
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-10-16 20:56:00 +0200

    Created Tables and ApiResponse File

---
+++ Ecommerce_Api/Controllers/MenuItemController.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce_Api.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Api.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        //creating a private field that refernces the db
+        private readonly ApplicationDbContext _db;
+
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            return Ok(_db.MenuItems);
+        }
+    }
+}
--- Ecommerce_Api/Migrations/20241016184224_InitalSeedMenuItem.Designer.cs
+++ Ecommerce_Api/Migrations/20241016184224_InitalSeedMenuItem.Designer.cs
@@ -4,6 +4,7 @@ using Ecommerce_Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ecommerce_Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241016184224_InitalSeedMenuItem")]
+    partial class InitalSeedMenuItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
