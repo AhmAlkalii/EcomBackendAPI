53c49ebd7bae7236fdce511ffa2387952a794831
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-10-22 11:12:47 +0200

    Order Controller (Get Request) Done

---
+++ Ecommerce_Api/Controllers/OrderController.cs
@@ -0,0 +1,88 @@
+﻿using System.Net;
+using Ecommerce_Api.Data;
+using Ecommerce_Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        protected ApiResponse _response;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new();
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetOrders(string? userId)
+        {
+            try
+            {
+                var orderHeaders = _db.OrderHeaders
+                    .Include(u => u.OrderDetails)
+                    .ThenInclude(u => u.MenuItem).OrderByDescending(u => u.OrderHeaderId);
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    _response.Result = orderHeaders.Where(u => u.ApplicationUserId == userId);
+                }
+                else
+                {
+                    _response.Result = orderHeaders;
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>(){ex.ToString()};
+            }
+
+            return _response;
+        }
+        
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ApiResponse>> GetOrders(int id)
+        {
+            try
+            {
+                if (id == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+
+                }
+                var orderHeaders = _db.OrderHeaders
+                    .Include(u => u.OrderDetails)
+                    .ThenInclude(u => u.MenuItem).OrderByDescending(u => u.OrderHeaderId);
+                
+                if (orderHeaders == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+
+                }
+                _response.Result = orderHeaders;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>(){ex.ToString()};
+            }
+
+            return _response;
+        }
+    }
+}
--- Ecommerce_Api/Data/ApplicationDbContext.cs
+++ Ecommerce_Api/Data/ApplicationDbContext.cs
@@ -19,6 +19,8 @@ namespace Ecommerce_Api.Data
         public DbSet<MenuItem>  MenuItems{ get; set; }
         public DbSet<ShoppingCart>  ShoppingCarts{ get; set; }
         public DbSet<CartItem>  CartItems{ get; set; }
+        public DbSet<OrderHeader>  OrderHeaders{ get; set; }
+        public DbSet<OrderDetails>  OrderDetails{ get; set; }
 
         //here we will fill the MenuItems table with values it is called seeding
         protected override void OnModelCreating(ModelBuilder modelBuilder)
