5601dbc1572c4b7a94225cdc1d82a691e03c7b15
Ahmed Alkali Mohammed <hmkalkali@gmail.com>
2024-10-22 11:33:28 +0200

    Order CRUD Operations Completed

--- Ecommerce_Api/Models/Dto/OrderHeaderUpdateDTO.cs
+++ Ecommerce_Api/Models/Dto/OrderHeaderUpdateDTO.cs
@@ -6,8 +6,6 @@
         public string PickupName { get; set; }
         public string PickupPhoneNumber { get; set; }
         public string PickupEmail { get; set; }
-        public DateTime OrderDate { get; set; }
-
         public string StripePaymentIntentID { get; set; }
         public string Status { get; set; }
     }
